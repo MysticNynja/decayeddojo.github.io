b4403ee071741c2b69210954932a67233d258158
Will York <iamwillyork@outlook.com>
2023-06-19 23:33:38 -0500

    adding all pages for our site as well as updating some of the elements

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -3,7 +3,7 @@
     <div class="container flex items-center space-x-4 flex-col lg:flex-row">
         <div class="w-full lg:w-1/2 mb-10 lg:mb-0">
             <div class="sm:mr-12 mr-0 text-center relative">
-                <img src="/images/others/about-thumb.webp" alt="">
+                <img src="/images/others/about-players.webp" alt="">
                 <CoolLightBox
                     :items="items"
                     :index="index"
@@ -18,18 +18,12 @@
         </div>
         <div class="w-full lg:w-1/2">
             <div class="about_title lg:mb-6 mb-4">
-                <h5 class="text-primary text-xl uppercase font-bold pl-24 lg:mb-6 mb-4 relative before:absolute before:content-[''] before:left-0 before:top-1/2 before:-translate-y-1/2 before:transform before:h-1 before:bg-primary before:w-16">About Bonx Studio</h5>
-                <h2 class="text-white font-exo font-bold uppercase xl:text-title lg:text-5xl md:text-4xl sm:text-3xl text-2xl xl:leading-70 lg:leading-12 leading-10 max-w-sm md:max-w-xl lg:max-w-2xl"> bonx is the best gaming studio for all gamer. </h2>
+                <h5 class="text-primary text-xl uppercase font-bold pl-24 lg:mb-6 mb-4 relative before:absolute before:content-[''] before:left-0 before:top-1/2 before:-translate-y-1/2 before:transform before:h-1 before:bg-primary before:w-16">About Decayed Dojo</h5>
+                <h2 class="text-white font-exo font-bold uppercase xl:text-title lg:text-5xl md:text-4xl sm:text-3xl text-2xl xl:leading-70 lg:leading-12 leading-10 max-w-sm md:max-w-xl lg:max-w-2xl"> two lifetime friends, brothers in digital arms, play games poorly for your enjoyment. </h2>
             </div>
             <div class="about_desc mb-10">
-                <p>It is a long established fact that a reader will be distracted the readable content of page when looking at it layout the point using lorem Ipsum is that it has a more-or-less normal distribution.</p>
-                <p>It is a long established fact that a reader will be distracted the readable content of page when looking at it layout the point using lorem Ipsum is that it has a more-or-less normal distribution.</p>
-            </div>
-            <div class="about_btn">
-                <n-link to="/contact" class="group primary-btn opacity-100 transition-all" style="background-image:url(/images/others/btn-bg.webp)">
-                    {{btnName}}
-                    <img src="/images/icon/arrrow-icon.webp" alt="Arrow Icon" class="ml-3 w-5 h-5 group-hover:ml-4 transition-all">
-                </n-link>  
+                <p>The core team of Decayed Dojo have known each other for about 2 decades, and in those decades we have played everything from Superman 64 to Diablo 4.  We decided to take the plunge into digital content creation.</p>
+                <p>We really want to entertain, hang out more, and if we make some new friends along the way that would be great! This is our digital home and hopefully you can find something here that makes you entertained.  If not, glad you gave us a shot! </p>
             </div>
         </div>
     </div>
@@ -47,7 +41,7 @@ export default {
             btnName: "Play Now",
             items: [
                 {
-                    src: "https://www.youtube.com/watch?v=eS9Qm4AOOBY"
+                    src: "https://player.twitch.tv/?channel=sarleeg&parent=www.decayeddojo.com"
                 }
             ],
             index: null
