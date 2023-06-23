1720e6259631f6e318fbc069fb8ac8b78477b034
Will York <iamwillyork@outlook.com>
2023-06-23 17:00:16 -0500

    adding twitch social on footer

--- components/Footer/FooterInfoWidget.vue
+++ components/Footer/FooterInfoWidget.vue
@@ -11,7 +11,7 @@
         <!-- Text End -->
 
         <!-- Social Icon List Start -->
-        <ul class="flex mt-4">
+        <ul class="grid grid-cols-6 gap-2 ">
             <li v-for="(link, index) in links" :key="index" class="mr-4 last:mr-0">
                 <a :href="`${link.href}`" :style="{backgroundImage: `url(${ link.BgShape })`}" class="w-10 h-10 leading-10 text-center inline-block"><i :class="link.className"></i></a>
             </li>
@@ -26,19 +26,9 @@
             return {
                 links: [
                     {
-                        href: "https://www.facebook.com",
-                        BgShape: "/images/icon/facebook-bg.webp",
-                        className: "icofont-facebook text-white"
-                    },
-                    {
-                        href: "https://www.twitter.com",
-                        BgShape: "/images/icon/twittre.webp",
-                        className: "icofont-twitter text-white"
-                    },
-                    {
-                        href: "https://www.youtube.com",
-                        BgShape: "/images/icon/youtobe-bg.webp",
-                        className: "icofont-youtube-play text-white"
+                        href: "https://www.twitch.tv/decayeddojo",
+                        BgShape: "/images/icon/twitch-bg.webp",
+                        className: "icofont-twitch text-white"
                     }
                 ]
             }
