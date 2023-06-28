2a64a09164a0bcd72cf33a5814a0813608b0a288
Will York <iamwillyork@outlook.com>
2023-06-27 21:51:25 -0500

    fixing footer to have games

--- components/Footer/FooterWinnerWidget.vue
+++ components/Footer/FooterWinnerWidget.vue
@@ -19,34 +19,9 @@ export default {
             title: "Recent Games",
             games: [
                 {
-                    gamingBg: "/images/others/winners1.webp",
-                    gamingAlt: "Game",
+                    gamingBg: "/images/games/Fallout_76/icon.webp",
+                    gamingAlt: "Fallout 76",
                     slug: 1
-                },
-                {
-                    gamingBg: "/images/others/winners2.webp",
-                    gamingAlt: "Game",
-                    slug: 2
-                },
-                {
-                    gamingBg: "/images/others/winners3.webp",
-                    gamingAlt: "Game",
-                    slug: 3
-                },
-                {
-                    gamingBg: "/images/others/winners4.webp",
-                    gamingAlt: "Game",
-                    slug: 4
-                },
-                {
-                    gamingBg: "/images/others/winners5.webp",
-                    gamingAlt: "Game",
-                    slug: 5
-                },
-                {
-                    gamingBg: "/images/others/winners6.webp",
-                    gamingAlt: "Game",
-                    slug: 6
                 }
             ]
         }
