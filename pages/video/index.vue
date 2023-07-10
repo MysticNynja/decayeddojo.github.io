43f34c8b102ad2e4fd7fdc52bd1b7b92880725ca
Will York <iamwillyork@outlook.com>
2023-07-10 13:06:08 -0500

    Adding Video index and slugs

--- pages/video/index.vue
+++ pages/video/index.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="bg-cover bg-gray-800" style="background-image:url(/images/bg/body-bg.webp);">
-      <HeaderSection/>
+      <HeaderSection />
       <OffCanvasMobileMenu :class="{'show-mobile-menu' : navOpen}" @togglenav="navOpen = !navOpen" />
+      <Breadcrumb :BreadcrumbTitle="BreadcrumbTitle" :BreadcrumbSubTitle="BreadcrumbSubTitle" :BreadcrumbParent="BreadCrumbParent" :BreadcrumbParentURL="BreadcrumbParentURL"/>
+      <AllVideos />
+      <Footer />
     </div>
   </template>
   
   <script>
   export default {
     components: {
-        HeaderSection: () => import('@/components/HeaderSection'),
-        OffCanvasMobileMenu: () => import('@/components/Header/OffCanvasMobileMenu'),
+      HeaderSection: () => import("@/components/HeaderSection"),
+      OffCanvasMobileMenu: () => import("@/components/Header/OffCanvasMobileMenu"),
+      Breadcrumb: () => import("@/components/Breadcrumb"),
+      Footer: () => import("@/components/Footer"),
+      AllVideos: () => import("@/components/Videos/AllVideos"),
     }, 
     data() {
         return {
             navOpen: false,
-            paddingTop: "pt-32"
+            paddingTop: "pt-32",
+            BreadcrumbTitle: "Our Latest Videos",
+            BreadcrumbSubTitle: "All Videos",
+            BreadCrumbParent: "Home",
+            BreadcrumbParentURL: "/",
         }
     },
   }
