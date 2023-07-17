0a76563abdf38623188b7873d754e40d92434964
Will York <iamwillyork@outlook.com>
2023-07-17 11:44:59 -0500

    remove docs

--- docs/_nuxt/dff5f1f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{336:function(n,r,e){"use strict";e.r(r);e(14),e(22),e(23);var o={components:{error:function(){return Promise.resolve().then(e.bind(null,40))}}},t=e(7),component=Object(t.a)(o,(function(){var n=this._self._c;return n("div",[n("error")],1)}),[],!1,null,null,null);r.default=component.exports}}]);
